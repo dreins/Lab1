e7c2683bc773e8ccbac0f7bb418fc94845c38b2b
dreins <davynreinhard@gmail.com>
2022-09-07 14:07:02 +0700

    Lab 01 Done

---
+++ wishlist/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Kak Cinoy'
+}
+
+def show_wishlist(request):
+    return render(request, "wishlist.html", context)
+
+
+# Create your views here.
