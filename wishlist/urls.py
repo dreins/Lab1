5ce6d3ed11be698c6a447dd3a954df67f87b1ba3
dreins <davynreinhard@gmail.com>
2022-10-12 17:12:16 +0700

    Lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_ajax
+from wishlist.views import wishlist_form
 from wishlist.views import show_wishlist_xml
 from wishlist.views import show_wishlist_json
 from wishlist.views import show_wishlist_json_id
@@ -10,6 +12,8 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
+    path('ajax/submit/', wishlist_form, name='wishlist_form'),
     path('xml/', show_wishlist_xml, name='show_wishlist_xml'),
     path('json/', show_wishlist_json, name='show_wishlist_json'),
     path('json/<int:id>', show_wishlist_json_id, name='show_wishlist_json_id'),
--- wishlist/views.py
+++ wishlist/views.py
@@ -73,5 +73,28 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 
+# Lab 05
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'username': request.COOKIES['username'],
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
+
+def wishlist_form(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('wishlist:login_user')
+    
+    context = {'form':form}
+    return render(request, 'wishlist_form.html', context)
 
 # Create your views here.
